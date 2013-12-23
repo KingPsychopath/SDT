754e4af67d3afd6077907a51bcc42bfbe9c67f5b
Liam Allan <mr.programmer@live.co.uk>
2013-12-23 00:52:03 +0000

    Release
    Not much to say here, add the CS file to your project and try it.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDT //Sharp Data Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Table t = new Table();
+            t.doQuery("column FirstName,SecondName,Age,id,info"); //Creating and naming the columns
+            t.doQuery("insert Liam¬Allan¬16¬!AI!¬Created this program."); //Inserting data and so on - BRB for a second
+            t.doQuery("update Age id = \"1\" \"10\""); //Also able to update data.
+            t.doQuery("insert Cameron¬Davies¬16¬!AI!¬He's a cool guy.");
+            t.doQuery("insert Connor¬Cumming¬14¬!AI!¬Has a lot of Steam games.");
+            t.doQuery("insert Adam¬Martin¬19¬!AI!¬He's an Aussie.");
+            t.doQuery("insert Sophie¬Hagon¬16¬!AI!¬She's my wonderful and beautiful girlfriend.");
+            String[] d;
+            foreach (String data in t.doQuery("select FirstName % \"\"").ToString().Split('}')) {
+                if (data != "")
+                {
+                    d = data.Split('¬');
+                    Console.WriteLine(d[t.getArrayNumber("FirstName")] + " " + d[t.getArrayNumber("SecondName")] + ", is " + d[t.getArrayNumber("Age")] + " years old and is ID " + d[t.getArrayNumber("id")] + ". " + d[t.getArrayNumber("info")]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Table.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDT
+{
+    class Table
+    {
+        public String cd = ""; //column details Name¬0}name¬1}
+        public String t = ""; //Rows - Starting from 0+ DATA¬MOREDATA}
+        public int rows = 0;
+
+        public int getArrayNumber(String name)
+        {
+            int outp = -1;
+            String[] d;
+            foreach (String data in cd.Split('}'))
+            {
+                d = data.Split('¬');
+                if (data.StartsWith(name + "¬"))
+                {
+                    outp = int.Parse(d[1]);
+                }
+            }
+            return outp;
+        }
+
+        public object doQuery(String query)
+        {
+            String[] words = query.Split(' ');
+            String[] speech = query.Split('"');
+            String[] d;
+            switch (words[0].ToLower())
+            {
+                case "select": //SELECT name (=, %) "Liam" || Select COLUMN (=, %) "VALUE"
+                    String outps = "";
+                    foreach (String row in t.Split('}'))
+                    {
+                        try
+                        {
+                            d = row.Split('¬');
+                            switch (words[2])
+                            {
+                                case "%":
+                                    if (d[getArrayNumber(words[1])].Contains(speech[1]))
+                                    {
+                                        outps += row + "}";
+                                    }
+                                    break;
+                                case "=":
+                                    if (d[getArrayNumber(words[1])] == speech[1])
+                                    {
+                                        outps += row + "}";
+                                    }
+                                    break;
+                            }
+                            
+                        }
+                        catch { }
+                    }
+                    return outps.Trim();
+                case "delete":
+                    foreach (String row in t.Split('}'))
+                    {
+                        try
+                        {
+                            d = row.Split('¬');
+                            switch (words[2])
+                            {
+                                case "%":
+                                    if (d[getArrayNumber(words[1])].Contains(speech[1]))
+                                    {
+                                        t = t.Replace(row + "}", "");
+                                    }
+                                    break;
+                                case "=":
+                                    if (d[getArrayNumber(words[1])] == speech[1])
+                                    {
+                                        t = t.Replace(row + "}", "");
+                                    }
+                                    break;
+                            }
+                            
+                        }
+                        catch { }
+                    }
+                    return true;
+                case "insert": //insert Liam¬Allan¬1997
+                    int a = 0; //Array number
+                    String str = "";
+                    String w;
+                    rows++;
+                    foreach (String word in words) {
+                        if (a != 0)
+                        {
+                            w = word.Replace("!AI!", rows.ToString());
+                            str += w + " ";
+                        }
+                        a++;
+                    }
+                    if (!str.EndsWith("¬")) { str += "¬"; }
+                    t += str + "}";
+                    return true;
+                case "row":
+                    int outpi = 0;
+                    foreach (String row in t.Split('}'))
+                    {
+                        try
+                        {
+                            d = row.Split('¬');
+                            switch (words[2])
+                            {
+                                case "%":
+                                    if (d[getArrayNumber(words[1])].Contains(speech[1]))
+                                    {
+                                        outpi++;
+                                    }
+                                    break;
+                                case "=":
+                                    if (d[getArrayNumber(words[1])] == speech[1])
+                                    {
+                                        outpi++;
+                                    }
+                                    break;
+                            }
+                        }
+                        catch { }
+                    }
+                    return outpi;
+                case "update":
+                    foreach (String row in t.Split('}'))
+                    {
+                        try
+                        {
+                            d = row.Split('¬');
+                            switch (words[3])
+                            {
+                                case "%":
+                                    if (d[getArrayNumber(words[2])].Contains(speech[1])) //update age FirstName = "Liam" "10"
+                                    {
+                                        t = t.Replace(d[getArrayNumber(words[1])] + "¬", speech[3] + "¬");
+                                    }
+                                    break;
+                                case "=":
+                                    if (d[getArrayNumber(words[2])] == speech[1])
+                                    {
+                                        t = t.Replace(d[getArrayNumber(words[1])] + "¬", speech[3] + "¬");
+                                    }
+                                    break;
+                            }
+                            
+                        }
+                        catch { }
+                    }
+                    return true;
+                case "column":
+                    int l = 0;
+                    foreach (String col in words[1].Split(','))
+                    {
+                        l = cd.Split('}').Length - 1;
+                        cd += col + "¬" + l + "}";
+                    }
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
